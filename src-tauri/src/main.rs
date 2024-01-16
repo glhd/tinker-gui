c80fab2fa4b6b753c430de1d9d0d2f0523a0b2f8
Chris Morrell <inxilpro@users.noreply.github.com>
2024-01-16 02:47:40 -0500

    Prep for menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 fn main() {
+	let context = tauri::generate_context!();
+	// let run_menu = CustomMenuItem::new("run".to_string(), "Run");
+	// let reload_menu = CustomMenuItem::new("reload".to_string(), "Reload");
+	// let tinker_submenu = Submenu::new("Tinker", Menu::new().add_item(run_menu).add_item(reload_menu));
+	// let menu = tauri::Menu::os_default(&context.package_info().name).add_submenu(tinker_submenu);
+
     tauri::Builder::default()
         .plugin(tauri_plugin_pty::init())
-        .run(tauri::generate_context!())
+        // .menu(menu)
+        .run(context)
         .expect("error while running tauri application");
 }
